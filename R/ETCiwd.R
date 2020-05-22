1f246c7760dee2ac9cdad46bda82845624a4c1e1
frajaroco <frajaroco@gmail.com>
2020-05-22 01:46:08 -0500

    Commit_2020-05-22_2

--- R/ETCiwd.R
+++ R/ETCiwd.R
@@ -1,33 +1,13 @@
-
-ETCiwd <- function(data.eto, farms, rastera, date.start, date.end=date.start){
+ETCiwd <- function(data.eto, farms, rastera, date.start, date.end=date.start, stat.coord){
 
 data.eto$date <- as.Date(data.eto$date)
 data.search <- subset(data.eto, date >= date.start & date <= date.end)
+data.stat <- sapply(1 : length(names(table(data.search$id.sta))), function(i) subset(data.search, id.stat == names(table(data.search$id.stat))[i]), simplify = FALSE)
+cumsum.data <- sapply(1 : length(data.stat), function(i) sum(data.stat[[i]]$eto))
+x <- stat.coord[,1]
+y <- stat.coord[,2]
+cumsum.eto <- data.frame(names(table(data.search$id.sta)),x,y,cumsum.data)
+names(cumsum.eto) <- c("id.stat","x","y","eto")
+coordinates(cumsum.eto) = ~ x + y
 
-data <- NULL
-for (i in 1 : length(names(table(data.search$id.sta)))){
-  data[[i]] = subset(data.search, id.sta == names(table(data.search$id.sta))[i])}
-
-return(data)}
-
-
-
-
-tapply(aa$eto,names(table(aa$id.sta)),subset)
-
-
-tapply(aa$eto,aa$id.sta,subset)
-
-datos1 = subset(data.eto, id_est == "Car")
-datos2 = subset(data.eto, id_est == "Rut")
-datos3 = subset(datos, id_est == "Sec")
-datos4 = subset(datos, id_est == "Zar")
-datos5 = subset(datos, id_est == "Pai")
-Estion = c("Car","Rut","Sec","Zar","Pai")
-
-
-
-
-
-
-Y <- ETCiwd(data.eto=XX, farms=Farms, rastera = RasterA, date.start = "2019-12-1")
+return(cumsum.eto)}
