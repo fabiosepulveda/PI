2f793f6e8a66a795856ffc554c8ffb418e76f2a4
frajaroco <frajaroco@gmail.com>
2020-05-22 19:19:00 -0500

    Commit_2020-05-22_6

--- R/plot.pi.R
+++ R/plot.pi.R
@@ -1,8 +1,8 @@
 plot.pi <- function(X,...){
 if (class(X)[1]=="SpatialPixelsDataFrame"){
-spplot(X,"var1.pred")
+spplot(X,"var1.pred",...)
 }else{
-spplot(X, c("pred.eto"))
+spplot(X, c("pred.eto"),...)
 }
 }
 getS3method("plot", "pi", optional = FALSE)
