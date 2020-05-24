62115ec4299163c5e97d0d058eff15cd0235e9d1
fabiosepulveda <fasepul@outlook.com>
2020-05-23 22:40:20 -0500

    2020-05-23-2

--- R/ETCiwd.R
+++ R/ETCiwd.R
@@ -1,7 +1,6 @@
 ETCiwd <- function(data.eto, farms, rastera, date.start, date.end=date.start, stat.coord){
-a= proj4string(farms) 
-b = proj4string(rastera)
-ifelse (a == b,"True","warning: your data don't have a coordinate reference system or don't have same coordinate reference system") 
+
+if (proj4string(farms) != proj4string(rastera)) stop("your data don't have a coordinate reference system (projected) or don't have same coordinate reference system") 
 
 data.eto$date <- as.Date(data.eto$date)
 data.search <- subset(data.eto, date >= date.start & date <= date.end)
