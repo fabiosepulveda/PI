a66ee659a9be2eba09d766f6c75da60b1e8340ff
frajaroco <frajaroco@gmail.com>
2020-05-28 01:39:56 -0500

    2020-05-28_1

--- R/ETCiwd.R
+++ R/ETCiwd.R
@@ -4,9 +4,14 @@ data.crop$date.crop <- as.Date(data.crop$date.crop)
 data.eto$date <- as.Date(data.eto$date)
 farms.crop.cons <- subset(data.crop, data.crop$date.crop <= cons.date)
 farms.name <- names(table(farms.crop.cons$farms.crop))
-farms.crop.date <- sapply(1:length(farms.name), function(i) subset(farms.crop.cons, farms.crop.cons$farms.crop == farms.name[i]), simplify = FALSE)
-
-aa <- sapply(1:length(farms.crop.date), function(i) names(table(farms.crop.date[[i]]$name.crop)), simplify = FALSE)
+farms.crop.date <- sapply(1:length(farms.name), function(i) subset(farms.crop.cons, 
+                   farms.crop.cons$farms.crop == farms.name[i]), simplify = FALSE)
+farms.date.name.crop <- sapply(1:length(farms.name), function(i) sapply(1:length(names(table(farms.crop.date[[i]]$name.crop))), 
+       function(j) subset(farms.crop.date[[i]],farms.crop.date[[i]]$name.crop==names(table(farms.crop.date[[i]]$name.crop)[j])),
+       simplify = FALSE))
+farms.date.crop <- sapply(1:length(farms.name), function(i) sapply(1:length(names(table(farms.crop.date[[i]]$name.crop))), 
+    function(j) farms.date.name.crop[[i]][[j]][which(farms.date.name.crop[[i]][[j]]$date.crop==max(farms.date.name.crop[[i]][[j]]$date.crop)),],
+    simplify = FALSE))
 
 ETO <- ETOiwd(data.eto=data.eto, farms=farms, rastera=rastera, date.start=cons.date, stat.coord=coord.stat)
 return(ETO)}
