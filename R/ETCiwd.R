6159fb9c670e41b4bcedde82da2449e99849c6de
fabiosepulveda <fasepul@outlook.com>
2020-05-28 22:05:29 -0500

    2020-05-28_2

--- R/ETCiwd.R
+++ R/ETCiwd.R
@@ -1,14 +1,18 @@
 ETCiwd <- function(data.crop, data.eto, farma, rastera, cons.date, stat.coord){
 
 data.crop$date.crop <- as.Date(data.crop$date.crop)
+data.crop$Age.crop = cons.date - data.crop$date.crop
 data.eto$date <- as.Date(data.eto$date)
-farms.crop.cons <- subset(data.crop, data.crop$date.crop <= cons.date)
+consultation = as.Date(cons.date)
+data.crop$Age.crop = consultation - data.crop$date.crop
+farms.crop.cons <- subset(data.crop, data.crop$date.crop <= consultation)
 farms.name <- names(table(farms.crop.cons$farms.crop))
 farms.crop.date <- sapply(1:length(farms.name), function(i) subset(farms.crop.cons, 
                    farms.crop.cons$farms.crop == farms.name[i]), simplify = FALSE)
 farms.date.name.crop <- sapply(1:length(farms.name), function(i) sapply(1:length(names(table(farms.crop.date[[i]]$name.crop))), 
        function(j) subset(farms.crop.date[[i]],farms.crop.date[[i]]$name.crop==names(table(farms.crop.date[[i]]$name.crop)[j])),
        simplify = FALSE))
+
 farms.date.crop <- sapply(1:length(farms.name), function(i) sapply(1:length(names(table(farms.crop.date[[i]]$name.crop))), 
     function(j) farms.date.name.crop[[i]][[j]][which(farms.date.name.crop[[i]][[j]]$date.crop==max(farms.date.name.crop[[i]][[j]]$date.crop)),],
     simplify = FALSE))
